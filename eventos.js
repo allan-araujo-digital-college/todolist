3db4a68e9644dfe192cd81310ea8d7c2500838dc
prof-otavio-medeiros <alessandrofeitoza01@gmail.com>
2022-09-27 08:49:28 -0300

    fetch get post

--- eventos.js
+++ eventos.js
@@ -6,16 +6,28 @@ function atualizarQuantidade() {
 
 
 function listarTarefas() {
-    let conteudo = lista_tarefas.sort().map(function (tarefa) {
-        return `
-            <div>
-                <input type="checkbox"> ${tarefa}
-            </div>
-        `;
-    });
+    fetch('https://62a4da7d47e6e4006399353b.mockapi.io/v1/tarefas1')
+        .then(function (resposta) {
+            return resposta.json();
+        })
+        .then(function (resposta) {
+            if (typeof resposta !== "string") {
+                let conteudo = resposta
+                    .map(function (tarefa) {
+                        return tarefa.titulo;
+                    })
+                    .sort()
+                    .map(function (tarefa) {
+                        return `
+                            <div>
+                                <input type="checkbox"> ${tarefa}
+                            </div>
+                        `;
+                    });
 
-    document.getElementById('tarefas').innerHTML = conteudo.join('');
-
+                document.getElementById('tarefas').innerHTML = conteudo.join('');
+            }
+        })
 }
 
 function addTarefa() {
@@ -23,20 +35,31 @@ function addTarefa() {
 
     let titulo = document.getElementById('input_nova_tarefa').value;
 
-    if (titulo.trim() === '') { 
+    if (titulo.trim() === '') {
         alert('Tarefa invalida');
         return;
     }
 
-    if (true === lista_tarefas.includes(titulo)) {
-        alert('Tarefa já existe');
-        return;
-    }
-
-    lista_tarefas.push(titulo);
-
-    document.getElementById('input_nova_tarefa').value = '';
-
-    atualizarQuantidade();
-    listarTarefas();
+    fetch('https://62a4da7d47e6e4006399353b.mockapi.io/v1/tarefas1', {
+        method: "POST",
+        body: JSON.stringify({
+            titulo,
+            prioridade: "baixa"
+        }),
+        headers: {
+            'Accept': 'application/json',
+            'Content-Type': 'application/json'
+        },
+    })
+        .then(function (resposta) {
+            return resposta.json();
+        })
+        .then(function (resposta) {
+            alert(JSON.stringify(resposta))
+            document.getElementById('input_nova_tarefa').value = '';
+            atualizarQuantidade();
+            listarTarefas();
+        })
 }
+
+listarTarefas()
